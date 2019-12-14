0fa4410c1218ec78cdad0188962ffab46d0bf8af
Caleb Willis <calebwillis50@yahoo.com>
2019-12-14 17:50:04 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -23,7 +23,8 @@ def third_challenge
           :name => "Mercutio"
           :age => "18"
           :attitude => "hot-headed"
-        }]
+        }
+      ]
    }, 
    :capulet => {
       :patriarch => {
